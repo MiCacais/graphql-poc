c79544618d5473bc03aeeef91bace155fb70a61a
Michelle Cacais <michelle_cacais@hotmail.com>
2021-06-03 17:13:49 -0400

    Added types and mutations

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,6 +6,20 @@ module Types
 
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
+    field :authors, [Types::AuthorType], null: true
+
+    def authors
+      Author.all
+    end
+
+    # /author/:id
+    field :author, Types::AuthorType, null: false do
+      argument :id, ID, required: true
+    end
+
+    def author(id:)
+      Author.find(id)
+    end
 
     # TODO: remove me
     field :test_field, String, null: false,
